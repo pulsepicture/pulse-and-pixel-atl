fc9f269257c7124933852f6e4662717dc9b0d446
pulsepicture <gomisphotography@gmail.com>
2025-10-07 22:27:03 -0400

    Create siteConfig.js

---
+++ src/siteConfig.js
@@ -0,0 +1,16 @@
+// src/siteConfig.js
+const siteConfig = {
+  brand: 'Pulse & Pixel ATL',
+  slogan: 'Make the night move.',
+  contact: {
+    email: 'contact@pulseandpixelatl.com',
+    phone: '+16788569504',
+    phoneDisplay: '(678) 856-9504',
+    instagramHandle: 'pictureadyatl',
+    instagramUrl: 'https://instagram.com/pictureadyatl',
+    address: '2200 Godby Rd, Atlanta, GA 30349',
+    mapsUrl: 'https://maps.google.com/?q=2200+Godby+Rd,+Atlanta,+GA+30349'
+  }
+};
+
+export default siteConfig;
